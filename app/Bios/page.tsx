4f655d241ef4f70b417f04c5d435be89b5c45125
lorigav <156033829+lorigav@users.noreply.github.com>
2026-07-17 15:22:51 -0700

    fix deployment bug

--- app/Bios/page.tsx
+++ app/Bios/page.tsx
@@ -17,7 +17,7 @@ const BioPage = () => {
             <h2 className="card-title">About Me</h2>
             <p>Hello! My name is Lori Gavia and I am a technical artist based in Los Angeles, CA. I am currently {currentAge} and studied Liberal Arts and Engineering at
               Cal Poly San Luis Obispo with concentations in Computer Graphics and Animation. A lot of the projects I have worked on in college revolve around game development 
-              game engine optimization, and 3d asset creation. Passionate about both art and technology, I strive to provide efficient technical support to creative teams. </p>
+              game engine optimization, and 3d asset creation. Passionate about both art and technology, I strive to provide efficient technical support to creative teams.
             </p>
             <Link href="/Contact">
               <div className="card-actions justify-end">
